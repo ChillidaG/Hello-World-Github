5f3f83efd6247d9b43bb9cf77d32d51a0bbc667b
francisco <franciscoch@google.com>
2022-08-02 11:27:12 +0000

    test trigger github

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hola Github')
+        self.response.write('Hola Github, from GCP')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
